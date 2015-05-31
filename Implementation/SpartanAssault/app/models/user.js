f8e0ea9602b7306c01e7f5a4dd133f155f469ced
Andrei Hălceanu <dante.ryuuzaki94@gmail.com>
2015-05-31 21:22:00 +0300

    Removed directory

--- Implementation/SpartanAssault/app/models/user.js
+++
@@ -1,8 +0,0 @@
-var bcrypt   = require('bcrypt-nodejs');
-var generateHash = function(pass) {
-	return bcrypt.hashSync(pass, bcrypt.genSaltSync(8), null);
-}
-
-module.exports = {
-	'hash' : generateHash,
-};
--- Implementation/SpartanAssault/config/database.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	'host' : 'localhost',
-	'user' : 'root',
-	'pass' : '',
-	'name' : 'spartandb',
-};
