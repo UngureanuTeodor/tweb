c597d519b922909a4046623aae37cf541624d337
Nazare Mihail <mihailnazare@yahoo.com>
2015-06-17 02:16:18 +0300

    update

--- Implementation/SpartanAssault/config/database.js
+++ Implementation/SpartanAssault/config/database.js
@@ -155,6 +155,23 @@ module.exports = function(app) {
 				}
 			});
 			
+			models.warehouse=db.define("warehouse", {
+				warehouseID: { type: 'number', key: true},
+				itemID:Number,
+				type:String,
+				defeats:Number,
+				draws:Number,
+				dmg_taken:Number,
+				dmg_dealt:Number,
+				gold_won:Number,
+				gold_lost:Number,
+				type: { type: 'enum', values: ['dungeon', 'arena'], key: true}
+			}, {
+				methods : {
+					
+				}
+			});
+			
 			db.sync();
 		}
 	}));
